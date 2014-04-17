88fe7d75db70267e1ecccd0c07441a39e94467ab
Wenbo <yobo.ywb@gmail.com>
2014-04-16 23:16:44 -0700

    first commit

--- BasicSpring/src/main/java/com/wenboy/basicspring/controller/OrderController.java
+++ BasicSpring/src/main/java/com/wenboy/basicspring/controller/OrderController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,8 +28,21 @@ public class OrderController {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String showMessage(@RequestParam(value="name", required=false) String name, Model model){
-		model.addAttribute("name", name);
+		model.addAttribute("message", name);
 		return "orders";
+	}
+	
+	@RequestMapping(value="/{orderId}", method=RequestMethod.GET)
+	public String showOrder(@PathVariable String orderId, Model model){
+		model.addAttribute("message", orderId);
+		return "orders";
+	}
+	
+	@RequestMapping(value="/{orderName:[a-z-]-{version:\\d\\.\\d\\.\\d}{extension:\\.[a-z]}}",
+			method=RequestMethod.GET)
+	public String showPreviousOrder(@PathVariable String orderName, @PathVariable String version,
+			@PathVariable String extension){
+		return null;
 	}
 	
 }
